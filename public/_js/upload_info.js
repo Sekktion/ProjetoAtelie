229d56774992eb005436eadee8a43ebc6d6e850b
Flávio Filho <65620069+flaviozno@users.noreply.github.com>
2020-07-22 16:42:06 -0300

    Add files via upload

---
+++ public/_js/upload_info.js
@@ -0,0 +1,18 @@
+function abrir(){
+    var file = document.getElementById('file').files[0];
+    var r = new FileReader();
+    r.onload = function(e){
+        var x = r.result; document.getElementById('img_up').src = x;
+    }
+    
+     if((innerWidth!=350)&&(innerHeight!=280)){
+         innerWidth=innerWidth-435;
+         innerHeight=innerHeight-461;
+         alert('As dimensões da sua imagem foram alteradas')
+         return true;
+         r.readAsDataURL(file);
+     }else{
+         return true;
+         r.readAsDataURL(file);
+     }
+    }
